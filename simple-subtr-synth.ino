5ce6653bfa0882126a6f51e01e6017e0908e7a23
chmanie <code@chmanie.com>
2022-07-06 23:24:24 +0200

    Add more control pots

--- simple-subtr-synth.ino
+++ simple-subtr-synth.ino
@@ -1,7 +1,7 @@
-// Title: moogladder
-// Description: Sweeps the cutoff with an lfo
+// Title: simple subtractive synth
+// Description: oscillators! filters! lfos! reverb!
 // Hardware: Daisy Seed
-// Author: Ben Sergentanis
+// Author: Chris Maniewski
 
 #include "DaisyDuino.h"
 
@@ -9,9 +9,12 @@ DaisyHardware hw;
 
 float filter_freq;
 float filter_res;
-float atten_fm;
 float verb_fback;
 float wet_amt;
+float lfo_freq;
+float lfo_amt;
+float osc_freq;
+float master_vol;
 
 static MoogLadder flt;
 static Oscillator osc, lfo;
@@ -21,20 +24,22 @@ void AudioCallback(float **in, float **out, size_t size) {
   float osc_sample, output, lfo_sample;
 
   for (size_t i = 0; i < size; i++) {
-    float rev_tail0, rev_tail1;
+    float rev_tail0, rev_tail1, flt_freq;
 
-    lfo_sample = lfo.Process();
+    // lfo_sample is between -0.5 and 0.5
+    lfo_sample = (lfo.Process() + 0.5) * 5000;
 
-    // lfo_sample is between -1 and 1
-    osc.SetFreq(200.0 + 100.0 * lfo_sample);
+    flt_freq = filter_freq * (1 - lfo_amt) + lfo_sample * lfo_amt;
+    flt.SetFreq(flt_freq);
+
     osc_sample = osc.Process();
 
     output = flt.Process(osc_sample);
 
     verb.Process(output, output, &rev_tail0, &rev_tail1);
 
-    out[0][i] = (1 - wet_amt) * output + wet_amt * rev_tail0;
-    out[1][i] = (1 - wet_amt) * output + wet_amt * rev_tail1;
+    out[0][i] = ((1 - wet_amt) * output + wet_amt * rev_tail0) * master_vol;
+    out[1][i] = ((1 - wet_amt) * output + wet_amt * rev_tail1) * master_vol;
   }
 }
 
@@ -51,13 +56,13 @@ void setup() {
   // set parameters for sine oscillator object
   lfo.Init(sample_rate);
   lfo.SetWaveform(Oscillator::WAVE_TRI);
-  lfo.SetAmp(1);
+  lfo.SetAmp(0.5);
   lfo.SetFreq(.4);
 
   // set parameters for sine oscillator object
   osc.Init(sample_rate);
   osc.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW);
-  osc.SetFreq(100);
+  osc.SetFreq(110);
   osc.SetAmp(0.25);
 
   // set reverb params
@@ -79,15 +84,22 @@ void setup() {
 
 void loop() {
   // TODO: use Arduino's map() function
+  lfo_freq = (1023.0 - analogRead(A2)) / 16.0f;
+  lfo.SetFreq(lfo_freq);
   // 0 - 5000.0
   filter_freq = (1023 - analogRead(A0)) / 1023.0 * 5000.0;
-  flt.SetFreq(filter_freq);
   // 0 - 0.8
   filter_res = (1023 - analogRead(A1)) / 1023.0 / 1.25;
   flt.SetRes(filter_res < 0.0 ? 0.0 : filter_res);
-  lfo.SetFreq(.4);
   // 0.4 - 1.0
   verb_fback = 0.4 + (1023 - analogRead(A5)) / 1023.0 * 0.6;
   verb.SetFeedback(verb_fback);
   wet_amt = (1023 - analogRead(A6)) / 1023.0;
+  // 110 - 2099
+  osc_freq = 2 * (1023 - analogRead(A3)) + 55;
+  osc.SetFreq(osc_freq);
+  // 0 - 1.0
+  lfo_amt = (1023 - analogRead(A4)) / 1023.0;
+  // 0 - 1.0
+  master_vol = (1023 - analogRead(A7)) / 1023.0;
 }

c380081637c84375c92ae048d850449b692fead6
Roey <91409567+roeytsemah@users.noreply.github.com>
2022-12-06 12:31:16 +0100

    Update simple-subtractive-synth.ino
    Added Simple x Daisy pinout cheatsheet

--- examples/simple-subtractive-synth/simple-subtractive-synth.ino
+++ examples/simple-subtractive-synth/simple-subtractive-synth.ino
@@ -15,6 +15,31 @@
  * A7 master volume
  */
 
+// SIMPLE X DAISY PINOUT CHEATSHEET
+//                    _____
+//                  ---------
+// 3v3          29  |       |   20    AGND
+// D15 / A0     30  |       |   19    OUT 01
+// D16 / A1     31  |       |   18    OUT 00
+// D17 / A2     32  |       |   17    IN 01
+// D18 / A3     33  |       |   16    IN 00
+// D19 / A4     34  |       |   15    D14
+// D20 / A5     35  |       |   14    D13
+// D21 / A6     36  |       |   13    D12
+// D22 / A7     37  |       |   12    D11
+// D23 / A8     38  |       |   11    D10
+// D24 / A9     39  |       |   10    D9
+// D25 / A10    40  |       |   09    D8
+// D26          41  |       |   08    D7
+// D27          42  |       |   07    D6
+// D28 / A11    43  |       |   06    D5
+// D29          44  |       |   05    D4
+// D30          45  |       |   04    D3
+// 3v3 Digital  46  |       |   03    D2
+// VIN          47  |       |   02    D1
+// DGND         48  |       |   01    D0
+//                  ---------
+
 #include "DaisyDuino.h"
 
 DaisyHardware hw;
